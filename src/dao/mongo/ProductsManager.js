c9b2556a9c0438997718ae9a864d4e5cba74dce6
arianaortigoza4 <arianaortigoza4@gmail.com>
2024-01-28 15:27:58 -0300

    Limit = 10 ok

--- src/dao/mongo/ProductsManager.js
+++ src/dao/mongo/ProductsManager.js
@@ -1,101 +1,86 @@
-const express = require('express')
-const {productsModel} = require('../models/products.model')
+const express = require('express');
+const { productsModel } = require('../models/products.model');
 
-const router       = express.Router()
-
+const router = express.Router();
 
 router
-    .post('/', async (req, res)=>{
+    .post('/', async (req, res) => {
         try {
-            const {title, description, code, price, status, stock, category, thumbnails } = req.body
-   
+            const { title, description, code, price, status, stock, category, thumbnails } = req.body;
+
             const productNew = {
                 title,
                 description,
                 code,
-                price, 
-                status, 
-                stock, 
-                category, 
+                price,
+                status,
+                stock,
+                category,
                 thumbnails
-            }
+            };
 
-    const result = await productsModel.create(productNew)
-            //console.log(result)
-            res.send({
-                stauts: 'success',
-                payload: result
-            })
-        } catch (error) {
-            res.status(500).send(`Error de server ${error.message}`)
-        }
-        // res.send('create carts')
-    })
-    .get('/', async (req, res)=>{
-        try {
-            const limit = req.query.limit;
-            //console.log("LIMITE = " + limit)
-            const products = await productsModel.find({})
+            const result = await productsModel.create(productNew);
             
-            //console.log(products)
-            res.send({
-                stauts: 'success',
-                payload: products
-            })
-        } catch (error) {
-            res.status(500).send(`Error de server ${error.message}`)
-        }
-    })
-    .get('/:pid', async (req, res)=>{
-        try {
-            const {pid} = req.params
-            const products = await productsModel.findOne({_id: pid})
-            res.send({
-                status: 'success',
-                payload: products
-            })
-        } catch (error) {
-            console.log(error)
-        }
-        // res.send('get cart')
-    })
-    .put('/:pid', async (req, res)=>{
-        try {
-            const {pid} = req.params
-            const bodyData = req.body;
-            const result = await productsModel.findOneAndUpdate({_id: pid}, bodyData, {new: true})
             res.send({
                 status: 'success',
                 payload: result
-            })
+            });
         } catch (error) {
-            console.log(error)
+            res.status(500).send(`Error de servidor: ${error.message}`);
         }
     })
-    .delete('/:pid', async (req, res)=>{
+    .get('/', async (req, res) => {
         try {
-            const {pid} = req.params
-            const result = await productsModel.findByIdAndDelete({_id: pid})
-            res.send({
-                status: 'success',
-                payload: result
-            })
-        } catch (error) {
-            console.log(error)
-        }
-    })
-    /*.post('/:cid/products/:pid', async (req, res)=>{
-        try {
-            const {cid, pid} = req.params // pid es el id de producto
-            const result = await productsService.addProductToCart(Number(cid), Number(pid))
-            res.send({
-                status: 'success',
-                payload: result
-            })
-        } catch (error) {
-            console.log(error)
-        }
-        
-    })*/
+            const { limit = 10, page = 1, sort, query } = req.query;
+            const products = await productsModel.find({}).limit(Number(limit)).skip((page - 1) * limit).sort(sort);
 
-module.exports = router
+            res.send({
+                status: 'success',
+                payload: products
+            });
+        } catch (error) {
+            res.status(500).send(`Error de servidor: ${error.message}`);
+        }
+    })
+    .get('/:pid', async (req, res) => {
+        try {
+            const { pid } = req.params;
+            const product = await productsModel.findOne({ _id: pid });
+
+            res.send({
+                status: 'success',
+                payload: product
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    .put('/:pid', async (req, res) => {
+        try {
+            const { pid } = req.params;
+            const bodyData = req.body;
+            const result = await productsModel.findOneAndUpdate({ _id: pid }, bodyData, { new: true });
+
+            res.send({
+                status: 'success',
+                payload: result
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    .delete('/:pid', async (req, res) => {
+        try {
+            const { pid } = req.params;
+            const result = await productsModel.findByIdAndDelete({ _id: pid });
+
+            res.send({
+                status: 'success',
+                payload: result
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+module.exports = router;
